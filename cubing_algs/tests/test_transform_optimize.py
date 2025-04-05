6918f40373797948ecb617d35ab92ec3770b3992
Fantomas42 <fantomas42@gmail.com>
2025-04-05 22:13:42 +0200

    Testing optimize

---
+++ cubing_algs/tests/test_transform_optimize.py
@@ -0,0 +1,284 @@
+import unittest
+
+from cubing_algs.parsing import parse_moves
+from cubing_algs.transform.optimize import compress_moves
+from cubing_algs.transform.optimize import optimize_do_undo_moves
+from cubing_algs.transform.optimize import optimize_double_moves
+from cubing_algs.transform.optimize import optimize_repeat_three_moves
+from cubing_algs.transform.optimize import optimize_triple_moves
+
+
+class OptimizeTestCase(unittest.TestCase):
+
+    def test_optimize_repeat_three_moves(self):
+        provide = parse_moves('R R R')
+        expect = parse_moves("R'")
+
+        self.assertEqual(
+            optimize_repeat_three_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R' R'")
+        expect = parse_moves('R')
+
+        self.assertEqual(
+            optimize_repeat_three_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('R R R U')
+        expect = parse_moves("R' U")
+
+        self.assertEqual(
+            optimize_repeat_three_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R' R' U F")
+        expect = parse_moves('R U F')
+
+        self.assertEqual(
+            optimize_repeat_three_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("U F R' R' R' U F")
+        expect = parse_moves('U F R U F')
+
+        self.assertEqual(
+            optimize_repeat_three_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_optimize_do_undo_moves(self):
+        provide = parse_moves("R R'")
+        expect = []
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect,
+        )
+
+        provide = parse_moves("R' R")
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect,
+        )
+
+        provide = parse_moves("R R' U")
+        expect = parse_moves('U')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R U F")
+        expect = parse_moves('U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("U F R' R U F")
+        expect = parse_moves('U F U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_optimize_do_undo_double_moves(self):
+        provide = parse_moves("R R R' R'")
+        expect = []
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect,
+        )
+
+        provide = parse_moves("R' R' R R")
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect,
+        )
+
+        provide = parse_moves("R R R' R' U")
+        expect = parse_moves('U')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R' R R U F")
+        expect = parse_moves('U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("U F R' R' R R U F")
+        expect = parse_moves('U F U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_optimize_do_undo_double_double_moves(self):
+        provide = parse_moves('R2 R2')
+        expect = []
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect,
+        )
+
+        provide = parse_moves('R2 R2 U')
+        expect = parse_moves('U')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('R2 R2 U F')
+        expect = parse_moves('U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('U F R2 R2 U F')
+        expect = parse_moves('U F U F')
+
+        self.assertEqual(
+            optimize_do_undo_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_optimize_double_moves(self):
+        provide = parse_moves('R R')
+        expect = parse_moves('R2')
+
+        self.assertEqual(
+            optimize_double_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R'")
+        expect = parse_moves('R2')
+
+        self.assertEqual(
+            optimize_double_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('R R U')
+        expect = parse_moves('R2 U')
+
+        self.assertEqual(
+            optimize_double_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R' U F")
+        expect = parse_moves('R2 U F')
+
+        self.assertEqual(
+            optimize_double_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('U F R R U F')
+        expect = parse_moves('U F R2 U F')
+
+        self.assertEqual(
+            optimize_double_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_optimize_triple_moves(self):
+        provide = parse_moves('R R2')
+        expect = parse_moves("R'")
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R2")
+        expect = parse_moves('R')
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('R2 R')
+        expect = parse_moves("R'")
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R2 R'")
+        expect = parse_moves('R')
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R' R2 U")
+        expect = parse_moves('R U')
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R2 R' U F")
+        expect = parse_moves('R U F')
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves("R2 R' U F F")
+        expect = parse_moves('R U F F')
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+        provide = parse_moves('U F R2 R U F')
+        expect = parse_moves("U F R' U F")
+
+        self.assertEqual(
+            optimize_triple_moves(provide.moves),
+            expect.moves,
+        )
+
+    def test_compress_moves(self):
+        provide = parse_moves(
+            "U (R U2 R' U' R U' R') "
+            "(R U2 R' U' R U' R') "
+            "(R U2 R' U' R U' R')",
+        )
+        expect = parse_moves("U R U2 R' U' R U R' U' R U R' U' R U' R'")
+
+        self.assertEqual(
+            compress_moves(provide.moves),
+            expect.moves,
+        )
--- cubing_algs/transform/optimize.py
+++ cubing_algs/transform/optimize.py
@@ -1,4 +1,7 @@
-def optimize_repeat_three_moves(old_moves: list[str]) -> list[str]:
+from cubing_algs.move import Move
+
+
+def optimize_repeat_three_moves(old_moves: list[Move]) -> list[Move]:
     """ R, R, R --> R' """
     i = 0
     changed = False
@@ -6,7 +9,7 @@ def optimize_repeat_three_moves(old_moves: list[str]) -> list[str]:
 
     while i < len(moves) - 2:
         if moves[i] == moves[i + 1] == moves[i + 2]:
-            moves[i:i + 3] = [invert(moves[i])]
+            moves[i:i + 3] = [moves[i].inverted]
             changed = True
         else:
             i += 1
@@ -17,17 +20,19 @@ def optimize_repeat_three_moves(old_moves: list[str]) -> list[str]:
     return moves
 
 
-def optimize_do_undo_moves(old_moves: list[str]) -> list[str]:
-    """ R R' --> <nothing>, R R R' R' --> <nothing> """
+def optimize_do_undo_moves(old_moves: list[Move]) -> list[Move]:
+    """ R R' --> <nothing>,
+        R2 R2 --> <nothing>
+        R R R' R' --> <nothing>
+    """
     i = 0
     changed = False
     moves = list(old_moves)
 
     while i < len(moves) - 1:
-        if invert(moves[i]) == moves[i + 1] or (
+        if moves[i].inverted == moves[i + 1] or (
                 moves[i] == moves[i + 1]
-                and moves[i][-1] == '2'
-                and moves[i + 1][-1] == '2'
+                and moves[i].is_double
         ):
             moves[i:i + 2] = []
             changed = True
@@ -40,7 +45,7 @@ def optimize_do_undo_moves(old_moves: list[str]) -> list[str]:
     return moves
 
 
-def optimize_double_moves(old_moves: list[str]) -> list[str]:
+def optimize_double_moves(old_moves: list[Move]) -> list[Move]:
     """ R, R --> R2 """
     i = 0
     changed = False
@@ -48,7 +53,7 @@ def optimize_double_moves(old_moves: list[str]) -> list[str]:
 
     while i < len(moves) - 1:
         if moves[i] == moves[i + 1]:
-            moves[i:i + 2] = ['%s2' % moves[i][0]]
+            moves[i:i + 2] = [moves[i].doubled]
             changed = True
         else:
             i += 1
@@ -59,19 +64,19 @@ def optimize_double_moves(old_moves: list[str]) -> list[str]:
     return moves
 
 
-def optimize_triple_moves(old_moves: list[str]) -> list[str]:
+def optimize_triple_moves(old_moves: list[Move]) -> list[Move]:
     """ R, R2 --> R' """
     i = 0
     changed = False
     moves = list(old_moves)
 
     while i < len(moves) - 1:
-        if moves[i][0] == moves[i + 1][0]:
-            if moves[i][-1] == '2' and moves[i + 1][-1] != '2':
-                moves[i:i + 2] = [invert(moves[i + 1])]
+        if moves[i].base_move == moves[i + 1].base_move:
+            if moves[i].is_double and not moves[i + 1].is_double:
+                moves[i:i + 2] = [moves[i + 1].inverted]
                 changed = True
-            elif moves[i][-1] != '2' and moves[i + 1][-1] == '2':
-                moves[i:i + 2] = [invert(moves[i])]
+            elif not moves[i].is_double and moves[i + 1].is_double:
+                moves[i:i + 2] = [moves[i].inverted]
                 changed = True
             else:
                 i += 1
@@ -80,5 +85,28 @@ def optimize_triple_moves(old_moves: list[str]) -> list[str]:
 
     if changed:
         return optimize_triple_moves(moves)
+
+    return moves
+
+
+def compress_moves(old_moves: list[Move]) -> list[Move]:
+    moves = list(old_moves)
+
+    compressing = True
+    while compressing:
+        changed = False
+        for optimizer in (
+                optimize_do_undo_moves,
+                optimize_repeat_three_moves,
+                optimize_double_moves,
+                optimize_triple_moves,
+        ):
+            new_moves = optimizer(moves)
+            if new_moves != moves:
+                moves = new_moves
+                changed = True
+
+        if not changed:
+            compressing = False
 
     return moves
