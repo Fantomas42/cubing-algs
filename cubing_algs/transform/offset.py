989307d2de3949c28b2b26b13c7783211f74519d
Fantomas42 <fantomas42@gmail.com>
2025-04-06 11:55:39 +0200

    Fix degrip and offset

--- cubing_algs/transform/offset.py
+++ cubing_algs/transform/offset.py
@@ -1,4 +1,3 @@
-# ruff: noqa: ERA001
 from cubing_algs.constants import OFFSET_TABLE
 from cubing_algs.move import Move
 
@@ -9,27 +8,15 @@ def unrotate(old_moves: list[Move], rotation: str) -> list[Move]:
 
     for move in old_moves:
         if move in rotation_table:
-            moves.append(rotation_table[move])
-        elif move[0] in rotation_table and move[-1] == '2':
-            moves.append('%s2' % rotation_table[move[0]][0])
-        elif "%s'" % move[0] in rotation_table and move[-1] == '2':
-            moves.append('%s2' % rotation_table["%s'" % move[0]][0])
+            moves.append(Move(rotation_table[move]))
+        elif move.base_move in rotation_table and move.is_double:
+            moves.append(Move(rotation_table[move.base_move]).doubled)
+        elif Move(move.base_move).inverted in rotation_table and move.is_double:
+            moves.append(Move(rotation_table[Move(move.base_move).inverted]).doubled)
         elif move.inverted in rotation_table:
             moves.append(Move(rotation_table[move.inverted]).inverted)
         else:
             moves.append(move)
-
-    # for move in old_moves:
-    #     if move in rotation_table:
-    #         moves.append(rotation_table[move])
-    #     elif move.base_move in rotation_table and move.is_double:
-    #         moves.append(Move(rotation_table[move[0]]).doubled)
-    #     elif "%s'" % move.base_move in rotation_table and move.is_double:
-    #         moves.append('%s2' % rotation_table["%s'" % move.base_move][0])
-    #     elif move.inverted in rotation_table:
-    #         moves.append(Move(rotation_table[move.inverted]).inverted)
-    #     else:
-    #         moves.append(move)
 
     return moves
 
