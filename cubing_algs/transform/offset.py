c1ebfc0a5fca2185d75f7383dd6691b54ef6a549
Fantomas42 <fantomas42@gmail.com>
2025-04-11 09:26:45 +0200

    Review offsets

--- cubing_algs/transform/offset.py
+++ cubing_algs/transform/offset.py
@@ -21,37 +21,48 @@ def unrotate(old_moves: list[Move], rotation: str) -> list[Move]:
     return moves
 
 
+def offset_moves(
+        old_moves: list[Move],
+        rotation: str,
+        count: int = 1,
+) -> list[Move]:
+    result = old_moves
+    for _ in range(count):
+        result = unrotate(result, rotation)
+    return result
+
+
 def offset_x_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, "x'")
+    return offset_moves(old_moves, "x'")
 
 
 def offset_x2_moves(old_moves: list[Move]) -> list[Move]:
-    return offset_x_moves(offset_x_moves(old_moves))
+    return offset_moves(old_moves, 'x', 2)
 
 
 def offset_xprime_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, 'x')
+    return offset_moves(old_moves, 'x')
 
 
 def offset_y_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, "y'")
+    return offset_moves(old_moves, "y'")
 
 
 def offset_y2_moves(old_moves: list[Move]) -> list[Move]:
-    return offset_y_moves(offset_y_moves(old_moves))
+    return offset_moves(old_moves, 'y', 2)
 
 
 def offset_yprime_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, 'y')
+    return offset_moves(old_moves, 'y')
 
 
 def offset_z_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, "z'")
+    return offset_moves(old_moves, "z'")
 
 
 def offset_z2_moves(old_moves: list[Move]) -> list[Move]:
-    return offset_z_moves(offset_z_moves(old_moves))
+    return offset_moves(old_moves, 'z', 2)
 
 
 def offset_zprime_moves(old_moves: list[Move]) -> list[Move]:
-    return unrotate(old_moves, 'z')
+    return offset_moves(old_moves, 'z')
