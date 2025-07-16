04c5b1575eea74d2536bf4b3e140905b27bf81b2
Fantomas42 <fantomas42@gmail.com>
2025-07-16 23:25:39 +0200

    Add compress final rotations

--- cubing_algs/tests/test_transform_rotation.py
+++ cubing_algs/tests/test_transform_rotation.py
@@ -2,10 +2,11 @@ import unittest
 
 from cubing_algs.move import Move
 from cubing_algs.parsing import parse_moves
+from cubing_algs.transform.rotation import compress_final_rotations
 from cubing_algs.transform.rotation import remove_final_rotations
 
 
-class TransformRotationTestCase(unittest.TestCase):
+class TransformRemoveRotationTestCase(unittest.TestCase):
 
     def test_remove_final_rotations(self):
         provide = parse_moves('R2 F U x y2')
@@ -53,6 +54,65 @@ class TransformRotationTestCase(unittest.TestCase):
         expect = parse_moves('R2@1 F@2 U@3')
 
         result = remove_final_rotations(provide)
+
+        self.assertEqual(
+            result,
+            expect,
+        )
+
+        for m in result:
+            self.assertTrue(isinstance(m, Move))
+
+
+class TransformCompressRotationTestCase(unittest.TestCase):
+
+    def test_compress_final_rotations(self):
+        provide = parse_moves("R2 F x x x' x x x")
+        expect = parse_moves('R2 F')
+
+        result = compress_final_rotations(provide)
+
+        self.assertEqual(
+            result,
+            expect,
+        )
+
+        for m in result:
+            self.assertTrue(isinstance(m, Move))
+
+    def test_compress_final_rotations_empty(self):
+        provide = parse_moves('R2 F')
+        expect = parse_moves('R2 F')
+
+        result = compress_final_rotations(provide)
+
+        self.assertEqual(
+            result,
+            expect,
+        )
+
+        for m in result:
+            self.assertTrue(isinstance(m, Move))
+
+    def test_compress_final_rotations_timed(self):
+        provide = parse_moves("R2@1 F@2 x'@3 x@4")
+        expect = parse_moves('R2@1 F@2')
+
+        result = compress_final_rotations(provide)
+
+        self.assertEqual(
+            result,
+            expect,
+        )
+
+        for m in result:
+            self.assertTrue(isinstance(m, Move))
+
+    def test_compress_final_rotations_impair(self):
+        provide = parse_moves("R2 F x' x x'")
+        expect = parse_moves("R2 F x'")
+
+        result = compress_final_rotations(provide)
 
         self.assertEqual(
             result,
--- cubing_algs/transform/rotation.py
+++ cubing_algs/transform/rotation.py
@@ -1,14 +1,38 @@
 from cubing_algs.move import Move
+from cubing_algs.transform.size import compress_moves
 
 
 def remove_final_rotations(old_moves: list[Move]) -> list[Move]:
     moves = []
 
-    rotations = True
+    rotation = True
     for move in reversed(old_moves):
-        if rotations and (move.is_rotation_move or move.is_pause):
+        if rotation and (move.is_rotation_move or move.is_pause):
             continue
-        rotations = False
+        rotation = False
         moves.append(move)
 
     return list(reversed(moves))
+
+
+def compress_final_rotations(old_moves: list[Move]) -> list[Move]:
+    moves = []
+    rotations = []
+
+    rotation = True
+    for move in reversed(old_moves):
+        if rotation and (move.is_rotation_move or move.is_pause):
+            rotations.append(move)
+            continue
+        rotation = False
+        moves.append(move)
+
+    if not rotations:
+        return old_moves
+
+    moves.reverse()
+    rotations.reverse()
+
+    rotations = compress_moves(rotations)
+
+    return moves + rotations
