83d93cf6815c9ad18eb0c8974f964f06fc8a7cc3
Fantomas42 <fantomas42@gmail.com>
2025-08-31 01:02:51 +0200

    More tests

--- cubing_algs/tests/test_facelets.py
+++ cubing_algs/tests/test_facelets.py
@@ -23,6 +23,94 @@ class CubiesToFaceletsTestCase(unittest.TestCase):
             facelets,
         )
 
+    def test_cubies_to_facelets_solved_oriented(self):
+        cp = [0, 1, 2, 3, 4, 5, 6, 7]
+        co = [0, 0, 0, 0, 0, 0, 0, 0]
+        ep = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        eo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        so = [3, 4, 2, 0, 1, 5]
+        facelets = (
+            'DDDDDDDDD'
+            'LLLLLLLLL'
+            'FFFFFFFFF'
+            'UUUUUUUUU'
+            'RRRRRRRRR'
+            'BBBBBBBBB'
+        )
+
+        self.assertEqual(
+            cubies_to_facelets(
+                cp, co,
+                ep, eo,
+                so,
+            ),
+            facelets,
+        )
+
+    def test_cubies_to_facelets_solved_masked(self):
+        cp = [0, 1, 2, 3, 4, 5, 6, 7]
+        co = [0, 0, 0, 0, 0, 0, 0, 0]
+        ep = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        eo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        so = [0, 1, 2, 3, 4, 5]
+        facelets = (
+            'UUUUUUUUU'
+            'RRRRRR---'
+            'FFFFFF---'
+            '---------'
+            'LLLLLL---'
+            'BBBBBB---'
+        )
+        mask = (
+            '111111111'
+            '111111000'
+            '111111000'
+            '000000000'
+            '111111000'
+            '111111000'
+        )
+
+        self.assertEqual(
+            cubies_to_facelets(
+                cp, co,
+                ep, eo,
+                so, mask,
+            ),
+            facelets,
+        )
+
+    def test_cubies_to_facelets_solved_oriented_masked(self):
+        cp = [0, 1, 2, 3, 4, 5, 6, 7]
+        co = [0, 0, 0, 0, 0, 0, 0, 0]
+        ep = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        eo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        so = [3, 4, 2, 0, 1, 5]
+        facelets = (
+            '---------'
+            '---LLLLLL'
+            '---FFFFFF'
+            'UUUUUUUUU'
+            '---RRRRRR'
+            '---BBBBBB'
+        )
+        mask = (
+            '111111111'
+            '111111000'
+            '111111000'
+            '000000000'
+            '111111000'
+            '111111000'
+        )
+
+        self.assertEqual(
+            cubies_to_facelets(
+                cp, co,
+                ep, eo,
+                so, mask,
+            ),
+            facelets,
+        )
+
     def test_cubies_to_facelets(self):
         cp = [0, 5, 2, 1, 7, 4, 6, 3]
         co = [1, 2, 0, 2, 1, 1, 0, 2]
