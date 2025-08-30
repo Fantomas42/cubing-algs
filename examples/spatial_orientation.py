32d19034435dc3053bb54295fec8cfb5e93ab55b
Fantomas42 <fantomas42@gmail.com>
2025-08-30 14:44:38 +0200

    Demo of spatial orientation preservation working with Cubies

---
+++ examples/spatial_orientation.py
@@ -0,0 +1,16 @@
+"""
+Demonstrate preservation of the cube spatial orientation
+when serializing/deserializing facelets to cubies.
+"""
+from cubing_algs.vcube import VCube
+
+cube = VCube()
+cube.rotate('F R x')
+cube.show()
+
+print(cube)
+
+n_cube = VCube.from_cubies(*cube.to_cubies)
+n_cube.show()
+
+print(n_cube)
