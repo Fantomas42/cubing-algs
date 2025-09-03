9f3da813704568448379a4dc5581d1ef55920cf8
Fantomas42 <fantomas42@gmail.com>
2025-09-03 22:29:48 +0200

    Add scrambler

---
+++ cubing_algs/scrambler.py
@@ -0,0 +1,103 @@
+import math
+import re
+from random import choice
+from random import randint
+
+from cubing_algs.algorithm import Algorithm
+from cubing_algs.constants import FACE_ORDER
+from cubing_algs.constants import ITERATIONS_BY_CUBE_SIZE
+from cubing_algs.constants import OPPOSITE_FACES
+from cubing_algs.constants import OUTER_BASIC_MOVES
+from cubing_algs.parsing import parse_moves
+
+FACE_REGEXP = re.compile(rf"({ '|'.join(FACE_ORDER) })")
+
+MOVES_EASY_CROSS = [
+    'F',
+    'R',
+    'B',
+    'L',
+]
+
+
+def build_cube_move_set(cube_size: int) -> list[str]:
+    moves = []
+
+    for face in OUTER_BASIC_MOVES:
+        moves.extend(
+            [
+                face,
+                f"{ face }'",
+                f'{ face }2',
+            ],
+        )
+        if cube_size > 3:
+            moves.extend(
+                [
+                    f'{ face }w',
+                    f"{ face }w'",
+                    f'{ face }w2',
+                ],
+            )
+            if cube_size > 5:  # TODO(me): change
+                for i in range(2, math.ceil(cube_size / 2) + 1):
+                    moves.extend(
+                        [
+                            f'{ i }{ face }',
+                            f"{ i }{ face }'",
+                            f'{ i }{ face }2',
+                            f'{ i }{ face }w',
+                            f"{ i }{ face }w'",
+                            f'{ i }{ face }w2',
+                        ],
+                    )
+                # TODO(me): implement inner layers
+
+    return moves
+
+
+def is_valid_next_move(current: str, previous: str) -> bool:
+    current_move_search = FACE_REGEXP.search(current)
+    previous_move_search = FACE_REGEXP.search(previous)
+
+    if not current_move_search or not previous_move_search:
+        return False
+
+    current_move = current_move_search[0]
+    previous_move = previous_move_search[0]
+
+    if current_move == previous_move:
+        return False
+
+    return OPPOSITE_FACES[current_move] != previous_move
+
+
+def random_moves(cube_size: int,
+                 move_set: list[str],
+                 iterations: int) -> Algorithm:
+    value = choice(move_set)
+    moves = [value]
+    previous = value
+
+    if not iterations:
+        iterations_range = ITERATIONS_BY_CUBE_SIZE[min(cube_size, 7)]
+        iterations = randint(*iterations_range)
+
+    while len(moves) < iterations:
+        while not is_valid_next_move(value, previous):
+            value = choice(move_set)
+
+        previous = value
+        moves.append(value)
+
+    return parse_moves(moves)
+
+
+def scramble(cube_size: int, iterations: int = 0) -> Algorithm:
+    move_set = build_cube_move_set(cube_size)
+
+    return random_moves(cube_size, move_set, iterations)
+
+
+def scramble_easy_cross() -> Algorithm:
+    return random_moves(3, MOVES_EASY_CROSS, 10)
---
+++ cubing_algs/tests/test_scrambler.py
@@ -0,0 +1,297 @@
+import unittest
+
+from cubing_algs.scrambler import build_cube_move_set
+from cubing_algs.scrambler import is_valid_next_move
+from cubing_algs.scrambler import random_moves
+from cubing_algs.scrambler import scramble
+from cubing_algs.scrambler import scramble_easy_cross
+
+
+class TestValidNextMove(unittest.TestCase):
+
+    def test_is_valid_next_move_valid(self):
+        """Test that valid next moves are recognized."""
+        self.assertTrue(is_valid_next_move('F', 'R'))
+        self.assertTrue(is_valid_next_move("F'", 'R'))
+        self.assertTrue(is_valid_next_move('F2', "R'"))
+
+    def test_is_valid_next_move_invalid_same_face(self):
+        """Test that moves on the same face are invalid."""
+        self.assertFalse(is_valid_next_move('F', 'F'))
+        self.assertFalse(is_valid_next_move('F', "F'"))
+        self.assertFalse(is_valid_next_move('F2', 'F'))
+
+    def test_is_valid_next_move_invalid_none(self):
+        """Test that moves does not matche."""
+        self.assertFalse(is_valid_next_move('', 'F'))
+        self.assertFalse(is_valid_next_move('F', ''))
+        self.assertFalse(is_valid_next_move('Z', 'F'))
+
+    def test_is_valid_next_move_invalid_opposite_faces(self):
+        """Test that moves on opposite faces are invalid."""
+        self.assertFalse(is_valid_next_move('F', 'B'))
+        self.assertFalse(is_valid_next_move('R', 'L'))
+        self.assertFalse(is_valid_next_move('U', 'D'))
+
+    def test_is_valid_next_move_with_modifiers(self):
+        """Test that modifiers don't affect face validation."""
+        self.assertFalse(is_valid_next_move('Fw', 'F'))
+        self.assertFalse(is_valid_next_move('Fw', 'B'))
+
+
+class TestCubeMoveSet(unittest.TestCase):
+    maxDiff = None
+
+    def test_build_cube_move_set_2x2x2(self):
+        self.assertEqual(
+            build_cube_move_set(2),
+            [
+                'R', "R'", 'R2',
+                'F', "F'", 'F2',
+                'U', "U'", 'U2',
+                'L', "L'", 'L2',
+                'B', "B'", 'B2',
+                'D', "D'", 'D2',
+            ],
+        )
+
+    def test_build_cube_move_set_3x3x3(self):
+        self.assertEqual(
+            build_cube_move_set(3),
+            [
+                'R', "R'", 'R2',
+                'F', "F'", 'F2',
+                'U', "U'", 'U2',
+                'L', "L'", 'L2',
+                'B', "B'", 'B2',
+                'D', "D'", 'D2',
+            ],
+        )
+
+    def test_build_cube_move_set_4x4x4(self):
+        self.assertEqual(
+            build_cube_move_set(4),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+            ],
+        )
+
+    def test_build_cube_move_set_5x5x5(self):
+        self.assertEqual(
+            build_cube_move_set(5),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+            ],
+        )
+
+    def test_build_cube_move_set_6x6x6(self):
+        self.assertEqual(
+            build_cube_move_set(6),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                '2R', "2R'", '2R2', '2Rw', "2Rw'", '2Rw2',
+                '3R', "3R'", '3R2', '3Rw', "3Rw'", '3Rw2',
+
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                '2F', "2F'", '2F2', '2Fw', "2Fw'", '2Fw2',
+                '3F', "3F'", '3F2', '3Fw', "3Fw'", '3Fw2',
+
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                '2U', "2U'", '2U2', '2Uw', "2Uw'", '2Uw2',
+                '3U', "3U'", '3U2', '3Uw', "3Uw'", '3Uw2',
+
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                '2L', "2L'", '2L2', '2Lw', "2Lw'", '2Lw2',
+                '3L', "3L'", '3L2', '3Lw', "3Lw'", '3Lw2',
+
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                '2B', "2B'", '2B2', '2Bw', "2Bw'", '2Bw2',
+                '3B', "3B'", '3B2', '3Bw', "3Bw'", '3Bw2',
+
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+                '2D', "2D'", '2D2', '2Dw', "2Dw'", '2Dw2',
+                '3D', "3D'", '3D2', '3Dw', "3Dw'", '3Dw2',
+            ],
+        )
+
+    def test_build_cube_move_set_7x7x7(self):
+        self.assertEqual(
+            build_cube_move_set(7),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                '2R', "2R'", '2R2', '2Rw', "2Rw'", '2Rw2',
+                '3R', "3R'", '3R2', '3Rw', "3Rw'", '3Rw2',
+                '4R', "4R'", '4R2', '4Rw', "4Rw'", '4Rw2',
+
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                '2F', "2F'", '2F2', '2Fw', "2Fw'", '2Fw2',
+                '3F', "3F'", '3F2', '3Fw', "3Fw'", '3Fw2',
+                '4F', "4F'", '4F2', '4Fw', "4Fw'", '4Fw2',
+
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                '2U', "2U'", '2U2', '2Uw', "2Uw'", '2Uw2',
+                '3U', "3U'", '3U2', '3Uw', "3Uw'", '3Uw2',
+                '4U', "4U'", '4U2', '4Uw', "4Uw'", '4Uw2',
+
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                '2L', "2L'", '2L2', '2Lw', "2Lw'", '2Lw2',
+                '3L', "3L'", '3L2', '3Lw', "3Lw'", '3Lw2',
+                '4L', "4L'", '4L2', '4Lw', "4Lw'", '4Lw2',
+
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                '2B', "2B'", '2B2', '2Bw', "2Bw'", '2Bw2',
+                '3B', "3B'", '3B2', '3Bw', "3Bw'", '3Bw2',
+                '4B', "4B'", '4B2', '4Bw', "4Bw'", '4Bw2',
+
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+                '2D', "2D'", '2D2', '2Dw', "2Dw'", '2Dw2',
+                '3D', "3D'", '3D2', '3Dw', "3Dw'", '3Dw2',
+                '4D', "4D'", '4D2', '4Dw', "4Dw'", '4Dw2',
+            ],
+        )
+
+    def test_build_cube_move_set_8x8x8(self):
+        self.assertEqual(
+            build_cube_move_set(8),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                '2R', "2R'", '2R2', '2Rw', "2Rw'", '2Rw2',
+                '3R', "3R'", '3R2', '3Rw', "3Rw'", '3Rw2',
+                '4R', "4R'", '4R2', '4Rw', "4Rw'", '4Rw2',
+
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                '2F', "2F'", '2F2', '2Fw', "2Fw'", '2Fw2',
+                '3F', "3F'", '3F2', '3Fw', "3Fw'", '3Fw2',
+                '4F', "4F'", '4F2', '4Fw', "4Fw'", '4Fw2',
+
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                '2U', "2U'", '2U2', '2Uw', "2Uw'", '2Uw2',
+                '3U', "3U'", '3U2', '3Uw', "3Uw'", '3Uw2',
+                '4U', "4U'", '4U2', '4Uw', "4Uw'", '4Uw2',
+
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                '2L', "2L'", '2L2', '2Lw', "2Lw'", '2Lw2',
+                '3L', "3L'", '3L2', '3Lw', "3Lw'", '3Lw2',
+                '4L', "4L'", '4L2', '4Lw', "4Lw'", '4Lw2',
+
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                '2B', "2B'", '2B2', '2Bw', "2Bw'", '2Bw2',
+                '3B', "3B'", '3B2', '3Bw', "3Bw'", '3Bw2',
+                '4B', "4B'", '4B2', '4Bw', "4Bw'", '4Bw2',
+
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+                '2D', "2D'", '2D2', '2Dw', "2Dw'", '2Dw2',
+                '3D', "3D'", '3D2', '3Dw', "3Dw'", '3Dw2',
+                '4D', "4D'", '4D2', '4Dw', "4Dw'", '4Dw2',
+            ],
+        )
+
+    def test_build_cube_move_set_9x9x9(self):
+        self.assertEqual(
+            build_cube_move_set(9),
+            [
+                'R', "R'", 'R2', 'Rw', "Rw'", 'Rw2',
+                '2R', "2R'", '2R2', '2Rw', "2Rw'", '2Rw2',
+                '3R', "3R'", '3R2', '3Rw', "3Rw'", '3Rw2',
+                '4R', "4R'", '4R2', '4Rw', "4Rw'", '4Rw2',
+                '5R', "5R'", '5R2', '5Rw', "5Rw'", '5Rw2',
+
+                'F', "F'", 'F2', 'Fw', "Fw'", 'Fw2',
+                '2F', "2F'", '2F2', '2Fw', "2Fw'", '2Fw2',
+                '3F', "3F'", '3F2', '3Fw', "3Fw'", '3Fw2',
+                '4F', "4F'", '4F2', '4Fw', "4Fw'", '4Fw2',
+                '5F', "5F'", '5F2', '5Fw', "5Fw'", '5Fw2',
+
+                'U', "U'", 'U2', 'Uw', "Uw'", 'Uw2',
+                '2U', "2U'", '2U2', '2Uw', "2Uw'", '2Uw2',
+                '3U', "3U'", '3U2', '3Uw', "3Uw'", '3Uw2',
+                '4U', "4U'", '4U2', '4Uw', "4Uw'", '4Uw2',
+                '5U', "5U'", '5U2', '5Uw', "5Uw'", '5Uw2',
+
+                'L', "L'", 'L2', 'Lw', "Lw'", 'Lw2',
+                '2L', "2L'", '2L2', '2Lw', "2Lw'", '2Lw2',
+                '3L', "3L'", '3L2', '3Lw', "3Lw'", '3Lw2',
+                '4L', "4L'", '4L2', '4Lw', "4Lw'", '4Lw2',
+                '5L', "5L'", '5L2', '5Lw', "5Lw'", '5Lw2',
+
+                'B', "B'", 'B2', 'Bw', "Bw'", 'Bw2',
+                '2B', "2B'", '2B2', '2Bw', "2Bw'", '2Bw2',
+                '3B', "3B'", '3B2', '3Bw', "3Bw'", '3Bw2',
+                '4B', "4B'", '4B2', '4Bw', "4Bw'", '4Bw2',
+                '5B', "5B'", '5B2', '5Bw', "5Bw'", '5Bw2',
+
+                'D', "D'", 'D2', 'Dw', "Dw'", 'Dw2',
+                '2D', "2D'", '2D2', '2Dw', "2Dw'", '2Dw2',
+                '3D', "3D'", '3D2', '3Dw', "3Dw'", '3Dw2',
+                '4D', "4D'", '4D2', '4Dw', "4Dw'", '4Dw2',
+                '5D', "5D'", '5D2', '5Dw', "5Dw'", '5Dw2',
+            ],
+        )
+
+
+class TestRandomMoves(unittest.TestCase):
+
+    def test_random_moves_2x2x2(self):
+        moves = random_moves(2, ['F', 'R', 'U'], 0)
+
+        self.assertGreaterEqual(
+            len(moves), 9,
+        )
+
+        self.assertLessEqual(
+            len(moves), 11,
+        )
+
+    def test_random_moves_2x2x2_iterations(self):
+        moves = random_moves(2, ['F', 'R', 'U'], 5)
+
+        self.assertEqual(
+            len(moves), 5,
+        )
+
+
+class TestScramble(unittest.TestCase):
+
+    def test_scramble_3x3x3(self):
+        moves = scramble(3)
+
+        self.assertGreaterEqual(
+            len(moves), 25,
+        )
+
+        self.assertLessEqual(
+            len(moves), 30,
+        )
+
+    def test_scramble_3x3x3_iterations(self):
+        moves = scramble(3, 5)
+
+        self.assertEqual(
+            len(moves), 5,
+        )
+
+
+class TestScrambleEasyCross(unittest.TestCase):
+
+    def test_scramble_easy_cross(self):
+        moves = scramble_easy_cross()
+
+        self.assertEqual(
+            len(moves), 10,
+        )
+        self.assertTrue(
+            'U' not in moves,
+        )
+        self.assertTrue(
+            'D' not in moves,
+        )
