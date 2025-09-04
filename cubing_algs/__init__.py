358e228db4adfad0c9a713a307dfc3e35e48b71a
Fantomas42 <fantomas42@gmail.com>
2025-09-04 23:50:03 +0200

    Simplify __init__

--- cubing_algs/__init__.py
+++ cubing_algs/__init__.py
@@ -1,15 +1,1 @@
-from cubing_algs.algorithm import Algorithm
-from cubing_algs.exceptions import InvalidMoveError
-from cubing_algs.move import Move
-from cubing_algs.parsing import parse_moves
-from cubing_algs.parsing import parse_moves_cfop
-
 __version__ = '1.0.2'
-
-__all__ = [
-    'Algorithm',
-    'InvalidMoveError',
-    'Move',
-    'parse_moves',
-    'parse_moves_cfop',
-]
