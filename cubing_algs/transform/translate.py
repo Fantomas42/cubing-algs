7d010a048e31f6ebfdd939a7967c225b7c8f6761
Fantomas42 <fantomas42@gmail.com>
2025-10-18 14:15:30 +0200

    Early return

--- cubing_algs/transform/translate.py
+++ cubing_algs/transform/translate.py
@@ -13,13 +13,13 @@ def translate_moves(
     """
 
     def _translate_moves(algorithm: Algorithm) -> Algorithm:
+        if not orientation_moves or not algorithm:
+            return algorithm
+
         for orientation_move in orientation_moves:
             if not orientation_move.is_rotation_move:
                 msg = f'{ orientation_move } is not a rotation move'
                 raise InvalidMoveError(msg)
-
-        if not orientation_moves or not algorithm:
-            return algorithm
 
         for orientation_move in orientation_moves:
             algorithm = DEGRIP_FULL[str(orientation_move.inverted)](algorithm)
