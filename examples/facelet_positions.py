695914d6d95d4e891efdec54ff1b40959c761687
Fantomas42 <fantomas42@gmail.com>
2025-11-06 22:49:28 +0100

    Show facelet positions

---
+++ examples/facelet_positions.py
@@ -0,0 +1,24 @@
+"""Display facelets by their position (0-53)."""
+import sys
+
+from cubing_algs import VCube
+
+
+def show_facelets(pos: list[str]) -> None:
+    """
+    Prepare mask for displaying facelets.
+
+    Args:
+        pos: List of facelets
+
+    """
+    cube = VCube()
+    mask = ['1'] * 54
+
+    for p in pos:
+        mask[int(p)] = '0'
+
+    cube.show(mask=''.join(mask))
+
+
+show_facelets(sys.argv[1:])
--- examples/spatial_orientation.py
+++ examples/spatial_orientation.py
@@ -1,8 +1,8 @@
-# ruff: noqa: T201
 """
 Demonstrate preservation of the cube spatial orientation
 when serializing/deserializing facelets to cubies.
 """
+# ruff: noqa: T201
 from cubing_algs.vcube import VCube
 
 cube = VCube()
