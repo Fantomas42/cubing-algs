e9f77ac902657b93e2a96001f6481c71cab55361
Fantomas42 <fantomas42@gmail.com>
2026-03-09 14:51:49 +0100

    Optimize number of copies done in optimizers

--- cubing_algs/transform/size.py
+++ cubing_algs/transform/size.py
@@ -1,10 +1,10 @@
 """Algorithm compression and expansion transformations for move optimization."""
 from cubing_algs.algorithm import Algorithm
 from cubing_algs.constants import MAX_ITERATIONS
-from cubing_algs.transform.optimize import optimize_do_undo_moves
-from cubing_algs.transform.optimize import optimize_double_moves
-from cubing_algs.transform.optimize import optimize_repeat_three_moves
-from cubing_algs.transform.optimize import optimize_triple_moves
+from cubing_algs.transform.optimize import optimize_do_undo_moves_inplace
+from cubing_algs.transform.optimize import optimize_double_moves_inplace
+from cubing_algs.transform.optimize import optimize_repeat_three_moves_inplace
+from cubing_algs.transform.optimize import optimize_triple_moves_inplace
 
 
 def compress_moves(
@@ -31,10 +31,10 @@ def compress_moves(
         start_length = len(moves)
 
         for optimizer in (
-            optimize_do_undo_moves,
-            optimize_repeat_three_moves,
-            optimize_double_moves,
-            optimize_triple_moves,
+            optimize_do_undo_moves_inplace,
+            optimize_repeat_three_moves_inplace,
+            optimize_double_moves_inplace,
+            optimize_triple_moves_inplace,
         ):
             moves = optimizer(moves)
 
