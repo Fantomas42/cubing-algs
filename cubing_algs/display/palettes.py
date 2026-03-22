4aa9c4bf83821a326526d40f8c8aa93279cba854
Fantomas42 <fantomas42@gmail.com>
2026-03-22 20:46:23 +0100

    Add void palette

--- cubing_algs/display/palettes.py
+++ cubing_algs/display/palettes.py
@@ -402,6 +402,20 @@ PALETTES: dict[str, PaletteConfig] = {
         'adjacent_background': '#2AE464',
         'hidden_ansi': build_ansi_color('#000000', '#00F253'),
     },
+    'void': {
+        'faces': (
+            '#000000',
+            '#000000',
+            '#000000',
+            '#000000',
+            '#000000',
+            '#000000',
+        ),
+        'font': '#C200C2',
+        'masked_background': '#620062',
+        'adjacent_background': '#008282',
+        'hidden_ansi': build_ansi_color('#000000', '#00C2C2'),
+    },
     # Futuristic
     'cyberpunk': {
         'faces': (
