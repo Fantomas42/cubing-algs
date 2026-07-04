59d5c621f01afce76e39e5b1f137096e92e022bd
Fantomas42 <fantomas42@gmail.com>
2026-07-04 14:13:16 +0200

    Remove useless test

--- cubing_algs/tests/test_display_vcube.py
+++ cubing_algs/tests/test_display_vcube.py
@@ -188,14 +188,6 @@ class TestVCubeDisplay(unittest.TestCase):  # noqa: PLR0904
         lines = result.split('\n')
         self.assertEqual(len(lines), 10)
 
-    def test_display_f2l_ll(self) -> None:
-        """Test display f2l+ll."""
-        self.cube.rotate("z2 R U R' U' z2")
-
-        result = self.printer.display(mode='f2l+ll')
-        lines = result.split('\n')
-        self.assertEqual(len(lines), 10)
-
     def test_display_f2l_cll(self) -> None:
         """Test display f2l+cll."""
         self.cube.rotate("z2 R U R' U' z2")
